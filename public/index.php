6736d0d8376c110574f9a193f9bba6f7c96a8a20
angelly-nic <angellynichole@gmail.com>
2026-01-28 10:15:45 +0700

    wip: implement router app

--- public/index.php
+++ public/index.php
@@ -4,5 +4,11 @@ require_once './app/core/Router.php';
 use App\Core\Router;
 
 $router = new Router();
+
+//Register Routes
+$routes->add('GET','/students','StudentController','index');
+$routes->add('GET','/students/create','StudentController','create');
+$routes->add('GET','/students/{id}','StudentController','show');
+
 $router->run();
 ?>
