d1f4094db4598364e09579d09541be0b86f6fad2
angelly-nic <angellynichole@gmail.com>
2026-03-10 14:30:06 +0700

    wip: implement student index page

---
+++ app/views/students/create.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Siswa</title>
+</head>
+<body>
+    <h1>Tambah siswa</h1>
+    <p>Menampilkan Form Siswa</p>
+
+
+</body>
+</html>
