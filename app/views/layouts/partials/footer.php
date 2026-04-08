149072faa0afad496d0a1f8748a4f0c01ade56ba
angelly-nic <angellynichole@gmail.com>
2026-04-08 10:13:50 +0700

    feat: implement layout app

---
+++ app/views/layouts/partials/footer.php
@@ -0,0 +1,3 @@
+<footer class="bg-gray-600 text-white p-4 text-center">
+    2026 - SMK Kristen Immanuel Pontianak - Sistem Sekolah
+</footer>
---
+++ app/views/layouts/partials/header.php
@@ -0,0 +1,7 @@
+<header class="bg-blue-900 text-white">
+        <div class="flex items-center justify-between container mx-auto p-4">
+            <a class="font-bold" href="/students">Sistem Sekolah</a>
+            <a class="py-2 px-4 bg-white text-blue-900 font-bold rounded-lg" href="/students/create">+ Tambah Siswa</a>
+        </div>
+
+</header>
